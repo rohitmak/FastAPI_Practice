0df9d0386e54025a928496da16ef134623514ae1
Rohit Makwana <rmakwana060@gmail.com>
2025-04-22 21:41:21 +0530

    second commit

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-from typing import Union
 from fastapi import FastAPI
-from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
-
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return {"message": "Hello, World!"}
 
 @app.get("/items/{item_id}")
-def read_items(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def items_route(item_id:int):
+    return {"message": "Hello, World!", "item_id": item_id}
 
-@app.put("/items/{item_id}")
-def update_items(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get("/users/me")
+def get_user_me():
+    return {"message": "This is me!"}
+
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: str):
+    return {"message": f"This is user with id {user_id}!"}
+
+@app.get("/users")
+def get_users_01():
+    return ["Rohit", "Ishan"]
+
+@app.get("/users")
+def get_users_01():
+    return ["Virat", "Hardik"]
